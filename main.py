11166a87a5e5cc93d502b96e8377c9cac4090163
‘acast83’ <acast83@gmail.com>
2022-01-25 10:35:16 +0100

    file modified

--- main.py
+++ main.py
@@ -3,10 +3,12 @@ from blic_data import blic_dict
 from mondo_data import mondo_dict
 app = FastAPI()
 
+# root endpoint
 @app.get("/")
 def root():
     return{"Welcome":"to my Novak Djokovic 10 latest news API"}
 
+# main endpoint
 @app.get("/api")
 def news_api():
     
