db01cade7453abe4e2fc4deb03bf968c6ba31451
‘acast83’ <acast83@gmail.com>
2022-01-31 00:08:18 +0100

    file modified

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from fastapi import FastAPI,Path
-from scrape import blic_funct,mondo_funct
+from fastapi import FastAPI, Path
+from scrape import blic_funct, mondo_funct
 import concurrent.futures
 
 
@@ -8,28 +8,30 @@ app = FastAPI()
 # root endpoint
 @app.get("/")
 def root():
-    return{"Welcome":"to my Novak Djokovic 10 latest articles API"}
+    return {"Welcome": "to my Novak Djokovic 10 latest articles API"}
+
 
 # main endpoint
 @app.get("/api")
 def news_api():
     # multiprocessing
     with concurrent.futures.ProcessPoolExecutor() as executor:
-        
-        blic_json = executor.submit(blic_funct,5)
-        mondo_json = executor.submit(mondo_funct,5)
-        return {"Blic":blic_json.result(),"Mondo":mondo_json.result()}
+
+        blic_json = executor.submit(blic_funct, 5)
+        mondo_json = executor.submit(mondo_funct, 5)
+        return {"Blic": blic_json.result(), "Mondo": mondo_json.result()}
+
 
 # custom endpoint
 @app.get("/api/{custom_num}")
-def custom_news_api(custom_num:int=Path(5,
-    description="Please enter number of articles between 1 and 10",
-    gt=0, 
-    lt=11
-)):
+def custom_news_api(
+    custom_num: int = Path(
+        5, description="Please enter number of articles between 1 and 10", gt=0, lt=11
+    )
+):
     # multiprocessing
     with concurrent.futures.ProcessPoolExecutor() as executor:
-        
-        blic_json = executor.submit(blic_funct,custom_num)
-        mondo_json = executor.submit(mondo_funct,custom_num)
-        return {"Blic":blic_json.result(),"Mondo":mondo_json.result()}
+
+        blic_json = executor.submit(blic_funct, custom_num)
+        mondo_json = executor.submit(mondo_funct, custom_num)
+        return {"Blic": blic_json.result(), "Mondo": mondo_json.result()}
