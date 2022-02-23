9559a3dd4fc4f0e635e16ca4c133033e797d002d
‘acast83’ <acast83@gmail.com>
2022-02-23 23:28:25 +0100

    files modified

--- main.py
+++ main.py
@@ -1,20 +1,24 @@
+"""Main module"""
+import concurrent.futures
 from fastapi import FastAPI, Path
 from scrape import blic_funct, mondo_funct
-import concurrent.futures
 
 
 app = FastAPI()
 
-# root endpoint
+
 @app.get("/")
 def root():
+    """
+    root endpoint
+    """
     return {"Welcome": "to my Novak Djokovic 10 latest articles API"}
 
 
 # main endpoint
 @app.get("/api")
 def news_api():
-    # multiprocessing
+    """multiprocessing """
     with concurrent.futures.ProcessPoolExecutor() as executor:
 
         blic_json = executor.submit(blic_funct, 5)
@@ -29,7 +33,7 @@ def custom_news_api(
         5, description="Please enter number of articles between 1 and 10", gt=0, lt=11
     )
 ):
-    # multiprocessing
+    """multiprocessing"""
     with concurrent.futures.ProcessPoolExecutor() as executor:
 
         blic_json = executor.submit(blic_funct, custom_num)
--- scrape.py
+++ scrape.py
@@ -1,26 +1,28 @@
+"""Scrape module"""
+import datetime
 import requests
 from bs4 import BeautifulSoup
-import datetime
+
 
 session = requests.Session()
 
-# blic web scrape 
-def blic_funct(num):
 
+def blic_funct(num):
+    """blic web scrape """
     # url
     blic_url = "https://www.blic.rs/search?q=novak+djokovic"
 
-    page=session.get(blic_url)
-    soup=BeautifulSoup(page.content,'html.parser')
-    data= soup.find_all("div",{"class":"content-wrapper"})
+    page = session.get(blic_url)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    data = soup.find_all("div", {"class": "content-wrapper"})
 
     # output dictionary
     blic_dict = {}
-    n=1
+    count = 1
     index = 0
 
-    while index<20:
-        
+    while index < 20:
+
         # article title
         article_title = data[index].find("p").text
         if "Novak" or "novak" or "NOVAK" in article_title:
@@ -31,51 +33,51 @@ def blic_funct(num):
             article_dict["Date"] = article_date
 
             # adding article title to a dictionary
-            article_dict["Title"]=article_title
+            article_dict["Title"] = article_title
 
             # article hyperlink
-            article_link =data[index].find(href=True)['href']
+            article_link = data[index].find(href=True)['href']
             article_dict["Hyperlink"] = article_link
 
-            blic_dict[f'Article number {n}']=article_dict
-            n+=1
-            if n>num:
+            blic_dict[f'Article number {count}'] = article_dict
+            count += 1
+            if count > num:
                 break
-        index+=1
+        index += 1
     return blic_dict
 
 
-
-#mondo web scrape
 def mondo_funct(num):
+    """mondo web scrape"""
 
-    #url
+    # url
     mondo_url = "https://mondo.rs/Novak-Djokovic/tag13798/1"
 
-    page=session.get(mondo_url)
-    soup=BeautifulSoup(page.content,'html.parser')
-    data = soup.find_all("article",{"class":"news-wrapper"})
+    page = session.get(mondo_url)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    data = soup.find_all("article", {"class": "news-wrapper"})
 
-    #output dictionary
+    # output dictionary
     mondo_dict = {}
     for index in range(num):
         article_dict = {}
 
         # article date
-        article_date_data = data[index].find("p",{"class":"time"}).text.split()
+        article_date_data = data[index].find(
+            "p", {"class": "time"}).text.split()
         if "Pre" in article_date_data:
             article_date = datetime.date.today().strftime("%d.%m.%Y")
         else:
             article_date = str(article_date_data[-1])[:-1]
-        article_dict["Date"]=article_date
+        article_dict["Date"] = article_date
 
         # article title
-        article_title = data[index].find("h2",{"class":"title"}).text
-        article_dict["Title"]=article_title
+        article_title = data[index].find("h2", {"class": "title"}).text
+        article_dict["Title"] = article_title
 
         # article hyperlink
         article_hyperlink = data[index].find(href=True)['href']
-        article_dict["Hyperlink"]=article_hyperlink
-        
-        mondo_dict[f'Article number {index+1}']=article_dict
+        article_dict["Hyperlink"] = article_hyperlink
+
+        mondo_dict[f'Article number {index+1}'] = article_dict
     return mondo_dict
